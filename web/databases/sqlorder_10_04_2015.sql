77f6459cdfb1700579c5e42627214693c0579bad
myhoang <myhoang@myhoang-HP-ProBook-4430s>
2015-05-16 15:33:01 +0700

    User modules added!!

--- web/databases/sqlorder_10_04_2015.sql
+++ web/databases/sqlorder_10_04_2015.sql
@@ -1,22 +1,22 @@
 CREATE TABLE `app_order_details` (
-    `OrderID` INTEGER NOT NULL,
-    `ProductID` INTEGER NOT NULL,
-    `UnitPrice` DECIMAL(18,2) NOT NULL DEFAULT 0,
-    `Quantity` SMALLINT(2) NOT NULL DEFAULT 1,
-    `Discount` REAL(8,0) NOT NULL DEFAULT 0,
-    `ToTal` decimal(18,2),
-    CONSTRAINT `PK_Order Details` PRIMARY KEY (`OrderID`, `ProductID`)
+    `orderid` INTEGER NOT NULL,
+    `productid` INTEGER NOT NULL,
+    `unitprice` DECIMAL(18,2) NOT NULL DEFAULT 0,
+    `quantity` SMALLINT(2) NOT NULL DEFAULT 1,
+    `discount` REAL(8,0) NOT NULL DEFAULT 0,
+    `total` decimal(18,2),
+    CONSTRAINT `pk_order details` PRIMARY KEY (`orderid`, `productid`)
 );
 CREATE TABLE `app_orders` (
-    `OrderID` INTEGER NOT NULL AUTO_INCREMENT,
-    `CustomerID` integer,
-    `OrderDate` DATETIME,
-    `Status` int default 0,
-    `SumToTal` decimal(18,0),
-    CONSTRAINT `PK_Orders` PRIMARY KEY (`OrderID`)
+    `orderid` INTEGER NOT NULL AUTO_INCREMENT,
+    `customerid` integer,
+    `orderdate` DATETIME,
+    `status` int default 0,
+    `sumtotal` decimal(18,0),
+    CONSTRAINT `pk_orders` PRIMARY KEY (`orderid`)
 );
 CREATE TABLE `app_products`(
-	`Id` int not null auto_increment primary key,
+	`id` int not null auto_increment primary key,
     `name` varchar(100),
     `category` int,
     `price` decimal(18,2),
@@ -25,7 +25,7 @@ CREATE TABLE `app_products`(
     `created_by` int
 );
 CREATE TABLE `app_product_categories`(
-	`Id` int not null auto_increment primary key,
+	`id` int not null auto_increment primary key,
     `name` varchar(100),
     `descrition` text,
     `created` datetime,
