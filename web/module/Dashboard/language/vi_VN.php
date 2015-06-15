3bed02bd8b3774c9d1f7681eda64c5c516ad3462
Kimkhoadoan <doankimkhoa92@gmail.com>
2015-06-15 23:46:32 +0700

    Thay đổi Dashboard

--- web/module/Dashboard/language/vi_VN.php
+++ web/module/Dashboard/language/vi_VN.php
@@ -27,44 +27,41 @@ return array(
     'START_TIME_TITLE' => 'Lần truy cập cuối',
     'STATUS_TITLE' => 'Trạng thái',
     'ACTION_TITLE' => 'Thao tác',
-	'USER_GENERAL'
-	'USER_PROFILE'
-	'USER_PACKET'
-	'USER_ORDER'
-	'MESSAGE_WELLCOME'
-	'MUDOLE_NAME_TITLE'
-	'MODULE_ATRRIBUTE'
-	'MODULE_STATUS'
-	'MODULE_DESCRIPTION_TITLE'
-	'MODULE_ADD_BUTTON_TITLE'
-	'MODULE_CANCLE_BUTTON_TITLE'
-	'CATEGORY_NAME_TITLE'
-	'CATEGORY_DESC_TITLE'
-	'CATEGORY_MODULE_TITLE'
-	'MODULE_ADD_BUTTON_TITLE'
-	'MODULE_CANCLE_BUTTON_TITLE'
-	'PACKET_NAME_TITLE'
-	'FORM_SELECT_CATEGORY'
-	'PACKET_PRICE_TITLE'
-	'PACKET_UNIT_TITLE'
-	'PACKET_VALUE_TITLE'
-	"NO_RESULT"
-	'MODULE_SAVE_BUTTON_TITLE'
-	'MODULE_EDIT_BUTTON_TITLE'
-	'SELECT_EDIT'
-	'ID'
-	'MODULE_STATUS_ON'
-	'MODULE_STATUS_OFF'
-	'FULLNAME_TITLE'
-	'PASSWORD_TITLE'
-	'RETYPEPASSWORD_TITLE'
-	'EMAIL_TITLE'
-	'PHONE_TITLE'
-	'BUILDING_TITLE'
-	'ROOM_TITLE'
-	'REG_SUBMIT_TITLE'
-	'AUTO_SUBMIT_TITLE'
-	'PACKET_TITLE'
-	'SUBMIT_TITLE'
-	
+	'USER_GENERAL' => 'Thông tin chung',
+	'USER_PROFILE' => 'Hồ sơ cá nhân',
+	'USER_PACKET' => 'Gói dịch vụ',
+	'USER_ORDER' => 'Hóa đơn thanh toán',
+	'MESSAGE_WELLCOME' => 'Xin chào',
+	'MUDOLE_NAME_TITLE' => 'Tên module',
+	'MODULE_ATRRIBUTE' => 'Thuộc tính module',
+	'MODULE_STATUS' => 'Trạng thái',
+	'MODULE_DESCRIPTION_TITLE' => 'Mô tả',
+	'CATEGORY_NAME_TITLE' => 'Tên loại',
+	'CATEGORY_DESC_TITLE' => 'Mô tả',
+	'CATEGORY_MODULE_TITLE' => 'Loại module',
+	'MODULE_ADD_BUTTON_TITLE' => 'Thêm module',
+	'MODULE_CANCLE_BUTTON_TITLE' => 'Hủy bỏ',
+	'PACKET_NAME_TITLE' => 'Tên gói dịch vụ',
+	'FORM_SELECT_CATEGORY' => '',
+	'PACKET_PRICE_TITLE' => 'Giá',
+	'PACKET_UNIT_TITLE' => 'Đơn vị tính',
+	'PACKET_VALUE_TITLE' => 'Giá trị',
+	"NO_RESULT" => 'Không có kết quả',
+	'MODULE_SAVE_BUTTON_TITLE' => 'Lưu module',
+	'MODULE_EDIT_BUTTON_TITLE' => 'Sửa module',
+	'SELECT_EDIT' => 'Lựa chọn thay đổi',
+	'ID' => 'ID',
+	'MODULE_STATUS_ON' => 'Bật module',
+	'MODULE_STATUS_OFF' => 'Tắt module',
+	'FULLNAME_TITLE' => 'Họ và Tên',
+	'PASSWORD_TITLE' => 'Mật khẩu',
+	'RETYPEPASSWORD_TITLE' => 'Nhập lại mật khẩu',
+	'EMAIL_TITLE' => 'Email',
+	'PHONE_TITLE' => 'Số điện thoại',
+	'BUILDING_TITLE' => 'Tòa nhà',
+	'ROOM_TITLE' => 'Số phòng',
+	'REG_SUBMIT_TITLE' => 'Đăng kí',
+	'AUTO_SUBMIT_TITLE' => 'Tự động',
+	'PACKET_TITLE' => 'Tên gói dịch vụ',
+	'SUBMIT_TITLE' => 'Submit'
 );
