bf465c80fa21bf38d0dc0b4323feb216aa8fced9
Nguyễn Minh Hiếu <hieuit7@users.noreply.github.com>
2016-08-31 09:04:54 +0700

    #6

--- web/config/autoload/paypal.global.php
+++ web/config/autoload/paypal.global.php
@@ -1,9 +1,9 @@
 <?php
 return array(
     'speck-paypal-api' => array(
-        'username' => 'hieunguyenminh.93-facilitator_api1.gmail.com',
-        'password' => 'DLR5UHDZX2RCLPTQ',
-        'signature' => 'AVn3tlesAcIGkDq-0rf82ParPEUsAayP3LB2vLqgCbAIvuHXfdQ0Uoap',
+        'username' => '',
+        'password' => '',
+        'signature' => '',
 
         // Sandbox: https://api-3t.sandbox.paypal.com/nvp
         // Live: https://api-3t.paypal.com/nvp https://api-3t.sandbox.paypal.com/nvp
